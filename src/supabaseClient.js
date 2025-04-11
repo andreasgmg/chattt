ddfffea3b105fd6f95728734066b72c61d98ef36
Andreas GMG <andreas.glans91@gmail.com>
2025-04-11 12:35:43 +0200

    Security update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jzkbpylctswzafxioxjj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6a2JweWxjdHN3emFmeGlveGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDUzMjIsImV4cCI6MjA1OTg4MTMyMn0.vMCgicAu8A2Vr8qnKcd4RbaDE7WNnSdl54JcaNrlLOI' 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
